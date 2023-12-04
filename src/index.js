405448de4f3847e6ee8a08add01cd957d1de5a69
Nomcebo-coder-hub <nmthembu2797@gmail.com>
2023-12-05 00:08:46 +0200

    Changed apiKey in js

--- src/index.js
+++ src/index.js
@@ -40,7 +40,7 @@ function formatDate(date) {
 }
 
 function searchCity(city) {
-  let apiKey = "b2a5adcct04b33178913oc335f405433";
+  let apiKey = "5cbf1d9t5efcaf340a57dfbdd800o0d9";
   let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
   axios.get(apiUrl).then(displayWeather);
 }
