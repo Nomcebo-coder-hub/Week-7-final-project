8e4446512a4e0c23422dc179cfc6dd2a03bb847e
Nomcebo-coder-hub <nmthembu2797@gmail.com>
2023-12-04 11:55:10 +0200

    Added temperature element to js file

--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
 function displayWeather(response) {
+  let temperatureElement = document.querySelector("#temperature");
+  let temperature = response.data.temperature.current;
   let cityElement = document.querySelector("#city");
   let descriptionElement = document.querySelector("#description");
   let humidityElement = document.querySelector("#humidity");
@@ -12,6 +14,7 @@ function displayWeather(response) {
   descriptionElement.innerHTML = response.data.condition.description;
   humidityElement.innerHTML = `${response.data.temperature.humidity}%`;
   windSpeedElement.innerHTML = `${response.data.wind.speed}km/h`;
+  temperatureElement.innerHTML = Math.round(temperature);
   iconElement.innerHTML = `<img src="${response.data.condition.icon_url}" class="weather-app-icon" />`;
 }
 
